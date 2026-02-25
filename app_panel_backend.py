d9f022c1cec0242bdd35269a2d9734466987d083
izc05 <isiciozafracantos@gmail.com>
2026-02-25 06:17:47 +0100

    Fix panel entrypoint and frontend script path

--- app_panel_backend.py
+++ app_panel_backend.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('.', 'index_panel.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
